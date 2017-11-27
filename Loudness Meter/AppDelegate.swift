f3b9d3d5ca19f8b2a443cb38c271dc552aaad701
PhilippMatthes <github@philippmatth.es>
2017-11-27 18:31:24 +0100

    stash

--- Loudness Meter/AppDelegate.swift
+++ Loudness Meter/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftRater
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        SwiftRater.daysUntilPrompt = 1
+        SwiftRater.usesUntilPrompt = 3
+        SwiftRater.significantUsesUntilPrompt = 3
+        SwiftRater.daysBeforeReminding = 1
+        SwiftRater.showLaterButton = true
+        SwiftRater.appLaunched()
+        
         return true
     }
 
