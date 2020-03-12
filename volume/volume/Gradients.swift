8c7b0e176aa01abddfc43c2580b3a7dea92de702
PhilippMatthes <github@philippmatth.es>
2020-03-12 11:03:07 +0100

    Approach home view design

---
+++ volume/volume/Gradients.swift
@@ -0,0 +1,23 @@
+//
+//  Gradients.swift
+//  volume
+//
+//  Created by It's free real estate on 12.03.20.
+//  Copyright © 2020 Philipp Matthes. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+
+class Gradients {
+    static let clouds = Gradient(colors: [
+        Color(red: 236/255, green: 233/255, blue: 230/255),
+        Color.white
+    ])
+    
+    static let playingWithReds = Gradient(colors: [
+        Color(red: 211/255, green: 16/255, blue: 39/255),
+        Color(red: 234/255, green: 56/255, blue: 77/255),
+    ])
+}
